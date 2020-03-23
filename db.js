0b1c585063edb59aabff8bf14d7928f7c869057a
KevinTeran94 <55459776+KevinTeran94@users.noreply.github.com>
2020-03-22 21:14:42 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    db: 'mongodb://admin:admin@cluster0-shard-00-00-scvza.mongodb.net:27017,cluster0-shard-00-01-scvza.mongodb.net:27017,cluster0-shard-00-02-scvza.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+    //db link deleted for security reasons
+    db: ''
   };
